b3912e6ce68af3be4f55ee1ffd0f5a1d787c3022
Max <max@debian-fx>
2020-12-07 02:17:57 +0100

    Added initial scroll position

--- offsets.js
+++ offsets.js
@@ -1,2 +1,10 @@
-OFFSET_X = 100;
-OFFSET_Y = 100;
+OFFSET_X = -1025;
+OFFSET_Y = -919;
+// Web Position was 1144, 1228
+// MC Position was 119, -309
+// wp_new = (mc-wp)
+
+DEFAULT_SCROLL_X = 600;
+DEFAULT_SCROLL_Y = 800;
+//DEFAULT_SCROLL_X = 0;
+//DEFAULT_SCROLL_Y = 0;
