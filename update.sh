1060b3dd6369b5fcc8a9b7320182cc424e9e4d71
Max <max@debian-fx>
2020-12-07 02:17:10 +0100

    Added scaling on update

--- update.sh
+++ update.sh
@@ -8,3 +8,7 @@ LAST="${DATADIR}/export_${LAST}"
 # update map.png symlink
 echo "Update: Lastest image" ${LAST}
 ln -fs ${LAST} map.png
+convert ${LAST} -scale 200% map_2x.png
+convert ${LAST} -scale 300% map_3x.png
+convert ${LAST} -scale 400% map_4x.png
+#convert ${LAST} -scale 800% map_8x.png
