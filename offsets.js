7d2df4e751e87e9678d91834764a3dd244e6b725
Max <max@debian-fx>
2020-12-08 19:05:55 +0100

    Current offsets & added default markers

--- offsets.js
+++ offsets.js
@@ -1,6 +1,6 @@
-OFFSET_X = -512;
+OFFSET_X = -1024;
 OFFSET_Y = -2048;
-// Web Position was 631, 1739
+// Web Position was 1144, 1739
 // MC Position was 119, -309
 // offset = (mc-wp)
 
@@ -8,3 +8,9 @@ DEFAULT_SCROLL_X = 600;
 DEFAULT_SCROLL_Y = 800;
 //DEFAULT_SCROLL_X = 0;
 //DEFAULT_SCROLL_Y = 0;
+
+
+DEFAULT_MARKERS = [
+	{ name: "Calibration test point", coord_x: 119, coord_y: -309 },
+	{ name: "Spawn", coord_x: -139, coord_y: -212 }
+]
